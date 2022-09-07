3e8fd0282886fe05b15f5f397f8c9a1bcc7a4024
bennisondevadoss <bennisondevadoss@gmail.com>
2022-09-07 22:30:52 +0530

    lint error fixed

--- app/controllers/images.controller.ts
+++ app/controllers/images.controller.ts
@@ -7,7 +7,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 function createImage(req: FastifyRequest, reply: FastifyReply) {
   const { id: bookId } = req.params as { id: number };
   const currentUser: UserInstance = req.currentUser;
-  const file = req.file();
+  const file = req.file(); /* The variable file is promise */
   create(bookId, file, currentUser)
     .then(() => {
       reply.send({ message: 'File uploaded successfully' });
