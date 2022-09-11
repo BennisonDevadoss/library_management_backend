8e6db25aaeb18056e1edf62e29dc0b46a064f16d
bennisondevadoss <bennisondevadoss@gmail.com>
2022-09-11 14:58:12 +0530

    lint error fixed

--- app/models/comment/comment.model.attributes.ts
+++ app/models/comment/comment.model.attributes.ts
@@ -16,7 +16,7 @@ export const attributes = {
     validate: {
       notNull: {
         args: true,
-        msg: `comment can't be empty`
+        msg: 'comment can\'t be empty'
       }
     },
     book_id: {
@@ -29,7 +29,7 @@ export const attributes = {
       validate: {
         notNull: {
           args: true,
-          msg: `book_id can't be empty`
+          msg: 'book_id can\'t be empty'
         }
       }
     },
@@ -43,7 +43,7 @@ export const attributes = {
       validate: {
         notNull: {
           args: true,
-          msg: `user_id can't be empty`
+          msg: 'user_id can\'t be empty'
         }
       }
     },
